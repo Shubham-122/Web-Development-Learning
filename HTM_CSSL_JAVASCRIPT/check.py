32f9883787fc97c1cad3916a27d5c9cf45ca4a90
Shubham Sahai Saxena <sahaishubham1@gmail.com>
2024-08-07 00:56:36 +0530

    Learned the difference between var and let, const and why let was introduced and also learned about window.

---
+++ HTM_CSSL_JAVASCRIPT/check.py
@@ -0,0 +1,7 @@
+def check(n):
+    for i in range(n):
+        print(i)
+    print("----------")
+    print(i)
+
+check(12) 
--- HTM_CSSL_JAVASCRIPT/first.js
+++ HTM_CSSL_JAVASCRIPT/first.js
@@ -1,7 +1,23 @@
-function print(){
-    for (var i=0; i<23; i++){
-        // console.log(Math.pow(i,2))
-        console.log(i*2)
+// function print(){
+//     for (var i=0; i<23; i++){
+//         // console.log(Math.pow(i,2))
+//         console.log(i*2)
+//     }
+// }
+// print()
+
+function check_var(){
+    for(var i=0; i<12; i++){
+        console.log(i)
     }
+    console.log(i)
 }
-print()
+function check_let(){
+    for(let i=0; i<12; i++){
+        console.log(i)
+    }
+    // console.log(i) This will not run because this out of the braces in which let was used because let is braces scoped. 
+}
+
+// check_var()
+check_let()
