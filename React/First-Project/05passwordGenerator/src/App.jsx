577c33d2dd316edd5de3d90ea3238e598085582f
Shubham Sahai Saxena <sahaishubham1@gmail.com>
2024-11-14 16:25:28 +0530

    Worked on the Password Generator.

--- React/First-Project/05passwordGenerator/src/App.jsx
+++ React/First-Project/05passwordGenerator/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react'
-import './App.css'
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -29,7 +29,26 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-green-600 bg-gray-700'>Test</div>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-green-600 bg-gray-700'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
+          <button className='outline-none bg-cyan-400 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" min={6} max={100} value={length} className='cursor-pointer' onChange={(e) => {setLength(e.target.value)}} />
+            <label>Length: {length}</label>
+          </div>
+          
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" deafaultChecked={numberAllowed} id='numberInput' onChange={() => {setNumberAllowed((prev) => !prev)}}/>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
